9fa59a0121509ec3ec2baeafe675dd5216b1db7d
obblie <obblie@users.noreply.github.com>
2026-03-06 06:41:09 +0000

    deploy: fadac94c5a2366007bc662645ffd076ddca3ab8e

---
+++ _next/static/chunks/app/upgrade/page-6c0a6d5f8b6fe3be.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[969,184,571],{7176:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=7176)}),_N_E=n.O()}]);
